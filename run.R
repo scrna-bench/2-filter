21c9eaff67623549b7241fcee8efb8b31ca5920e
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-04-14 14:55:00 +0200

    get cli args right

--- run.R
+++ run.R
@@ -18,7 +18,7 @@ parser$add_argument("--name", dest="name", type="character", required=TRUE,
 parser$add_argument("--type", dest="type", type="character", help="type of filtering")
 
 # parameter for 'input_h5' (comes from 1st stage)
-parser$add_argument("--1-data.h5ad", dest="input_h5", type="character", help="input file")
+parser$add_argument("--rawdata.h5ad", dest="input_h5", type="character", help="input file")
 
 args <- parser$parse_args()
 
